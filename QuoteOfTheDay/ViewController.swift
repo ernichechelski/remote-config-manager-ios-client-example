e9473a8f6d3ec0840a84388107815a523ae1568b
Ernest Chechelski <ernichechelski@gmail.com>
2020-10-28 11:49:13 +0100

    Initial Commit

---
+++ QuoteOfTheDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuoteOfTheDay
+//
+//  Created by Ernest Chechelski on 28/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
