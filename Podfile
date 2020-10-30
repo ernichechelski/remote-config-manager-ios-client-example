f827ce7b41fc3a78d70aa82398a033061de59d48
Ernest Chechelski <ernichechelski@gmail.com>
2020-10-30 12:41:17 +0100

    Initial config for the project

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'QuoteOfTheDay' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for QuoteOfTheDay
+  pod 'Firebase/Analytics'
+  pod 'Firebase/RemoteConfig'
+
+  target 'QuoteOfTheDayTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'QuoteOfTheDayUITests' do
+    # Pods for testing
+  end
+
+end
--- QuoteOfTheDay/ViewController.swift
+++ QuoteOfTheDay/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import FirebaseRemoteConfig
 
-class ViewController: UIViewController {
+@inline(__always) func with<T>(_ target: T, block: (T) -> Void) -> T {
+    block(target)
+    return target
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        refresh()
+        mainLabel.isUserInteractionEnabled = true
+        mainLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(refresh)))
     }
 
-
+    @objc private func refresh() {
+        let remoteConfig = with(RemoteConfig.remoteConfig()) {
+            $0.configSettings = with(RemoteConfigSettings()) {
+                $0.minimumFetchInterval = 0
+            }
+        }
+        remoteConfig.fetchAndActivate { _, _ in
+            DispatchQueue.main.async { [weak self] in
+                self?.mainLabel.text = remoteConfig["text"].stringValue
+            }
+        }
+    }
 }
 
